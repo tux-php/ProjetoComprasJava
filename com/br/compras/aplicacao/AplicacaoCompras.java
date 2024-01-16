8c0d2c581bb0b345734b2a70b92a553b1763d881
tux-php <fernandomcp23@gmail.com>
2024-01-16 11:11:07 -0300

    refatorando projeto

--- com/br/compras/aplicacao/AplicacaoCompras.java
+++ com/br/compras/aplicacao/AplicacaoCompras.java
@@ -4,7 +4,6 @@ import com.br.compras.controller.CartaoCredito;
 import com.br.compras.controller.Produto;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class AplicacaoCompras {
